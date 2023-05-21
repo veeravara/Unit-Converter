2eacdcf736f516be1a607545c71f4eee433969a3
veeravara <vara.rae2022.gna@gmail.com>
2023-05-21 13:32:00 +0530

    delete feature added

--- index.js
+++ index.js
@@ -4,32 +4,35 @@
 1 kilogram = 2.204 pound
 */
 // GRABBING ELEMENTS
-const inputElement=document.getElementById("rectangle")
-const inputButton=document.getElementById("button")
+const inputElement = document.getElementById("rectangle");
+const inputButton = document.getElementById("button");
+const lengthConversion = document.getElementById("group1");
 
-//EVENT LISTNERS
-inputButton.addEventListener("click",inputButtonfunction)
+// EVENT LISTENERS
+inputButton.addEventListener("click", inputButtonFunction);
 
-//FUNCTIONS
-function inputButtonfunction(){
-    const inputValue=inputElement.value
-    function length(){
-        let value=inputValue;
-        let feet=value*3.281;
-        feet=feet.toFixed(3)
-        let meter=value/3.281;
-        meter=meter.toFixed(3)
-        console.log(feet)
-        console.log(meter)
-        let lengthConversion=document.getElementById("group1")
-        lengthConversion.classList.add("lengthclass")
-        const lengthElement=document.createElement("h2")
-        lengthElement.innerText=`Meters=${feet} and Feet=${meter}`
-        lengthConversion.appendChild(lengthElement)
-        
-    }
-    length()
+// FUNCTIONS
+function inputButtonFunction() {
+  // Clear previous values
+  lengthConversion.innerHTML = "";
+
+  const inputValue = inputElement.value;
+
+  function length() {
+    let value = inputValue;
+    let feet = (value * 3.281).toFixed(3);
+    let meter = (value / 3.281).toFixed(3);
+    console.log(feet);
+    console.log(meter);
+
+    let lengthElement = document.createElement("h2");
+    lengthElement.innerText = `Meters=${meter} and Feet=${feet}`;
+    lengthConversion.appendChild(lengthElement);
+  }
+
+  length();
 }
+
 
 
 
@@ -51,6 +54,34 @@ function mass(){
 }
 
 
-length()
-volume()
-mass()
+/*
+const inputElement=document.getElementById("rectangle")
+const inputButton=document.getElementById("button")
+
+//EVENT LISTNERS
+inputButton.addEventListener("click",inputButtonfunction)
+
+//FUNCTIONS
+function inputButtonfunction(){
+    lengthElement.innerText=""
+    const inputValue=inputElement.value
+    function length(){
+        
+        let value=inputValue;
+        let feet=value*3.281;
+        feet=feet.toFixed(3)
+        let meter=value/3.281;
+        meter=meter.toFixed(3)
+        console.log(feet)
+        console.log(meter)
+        let lengthConversion=document.getElementById("group1")
+        lengthConversion.classList.add("lengthclass")
+        let lengthElement=document.createElement("h2")
+        lengthElement.innerText=`Meters=${feet} and Feet=${meter}`
+        lengthConversion.appendChild(lengthElement)
+            
+    }
+    length()
+    
+}
+*/
